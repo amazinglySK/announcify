0a94ba198d2e06e67759aaed1136d671d1f05dcc
amazinglySK <shashwat07ktm@gmail.com>
2022-02-06 15:38:07 +0400

    Adding favicons and doing some refractoring

--- src/app/js/adminFuncs.js
+++ src/app/js/adminFuncs.js
@@ -1,3 +1,49 @@
+const createGroup = async (name) => {
+  try {
+    const data = { name };
+    const response = await fetch("/group/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const jsonRes = await response.json();
+    return jsonRes;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const joinGroup = async (code) => {
+  const response = await fetch(`/group/${code}/add`, {
+    method: "POST",
+    headers: { "Conent-Type": "application/json" },
+  });
+  const jsonRes = await response.json();
+  return jsonRes;
+};
+
+const createClass = async (data) => {
+  const res = await fetch("/api/class", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const jsonRes = await res.json();
+
+  console.log("Sending the request");
+
+  if (!res.ok) {
+    console.error("Something went wrong while calling the API");
+    return;
+  }
+
+  console.log("Class Added");
+
+  return jsonRes;
+};
 function createStudentCard(data) {
   let newCard = document.createElement("div");
   $(newCard).attr({
@@ -60,6 +106,19 @@ $(document).ready((e) => {
       $("#className").text(className);
     }
   });
+  $("#create_btn").on("click", (e) => {
+    e.preventDefault();
+    const group_name = $("#group_name").val();
+    createGroup(group_name)
+      .then((res) => {
+        console.log(res);
+        alert(res.message);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Oops an error occured");
+      });
+  });
   $("#submit_btn").on("click", (e) => {
     e.preventDefault();
     let data = {
@@ -72,6 +131,18 @@ $(document).ready((e) => {
     let new_student_card = createStudentCard(data);
     $("#student-container").append($(new_student_card));
     console.log("Done appending");
+  });
+  $("#join_btn").on("click", (e) => {
+    e.preventDefault();
+    const group_code = $("#group_code").val();
+    joinGroup(group_code)
+      .then((res) => {
+        alert(res.message);
+        window.location.href = res.redirect_url;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
   $("#sendMail").on("click", (e) => {
     e.preventDefault();
@@ -87,5 +158,24 @@ $(document).ready((e) => {
       }
     });
     studentsToRegister = [];
+  });
+  $("#add_btn").on("click", (e) => {
+    e.preventDefault();
+    const grade = $("#grade").val();
+    const section = $("#section").val();
+    const data = {
+      grade: grade,
+      section: section,
+      class_name: `${grade}-${section}`,
+    };
+
+    createClass(data).then((res) => {
+      if (res.redirect_url) {
+        alert("Successfully created the class");
+        window.location.href = res.redirect_url;
+        return;
+      }
+      alert("Oops.. something went wrong");
+    });
   });
 });
